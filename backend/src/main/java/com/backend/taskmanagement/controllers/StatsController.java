3c68dc0fa853a2492d42725ce6330189c225c1ef
Martín <martin.oviedo@usach.cl>
2026-05-18 12:55:25 -0400

    Stats skeleton and sector complete

---
+++ backend/src/main/java/com/backend/taskmanagement/controllers/StatsController.java
@@ -0,0 +1,12 @@
+package com.backend.taskmanagement.controllers;
+
+import com.backend.taskmanagement.services.StatsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/stats")
+public class StatsController {
+    
+}
---
+++ backend/src/main/java/com/backend/taskmanagement/services/StatsService.java
@@ -0,0 +1,5 @@
+package com.backend.taskmanagement.services;
+
+public class StatsService {
+    
+}
